3a1ecd4b19311f5071fb92ec52d38a752f7b6790
Godwin Opara <godwinopara62@yahoo.com>
2023-06-07 11:25:38 +0100

    fullstack open anecdotes project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selected, setSelected] = useState(0);
+	const [votes, setVotes] = useState(Array(8).fill(0));
+
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well.",
+	];
+
+	const randNumber = Math.floor(Math.random() * anecdotes.length);
+
+	const handleVoteClick = () => {
+		const newVotes = [...votes];
+		newVotes[selected] += 1;
+		setVotes(newVotes);
+	};
+
+	return (
+		<div className="App">
+			<div>
+				<h3>Anecdotes</h3>
+				<div>{anecdotes[selected]}</div>
+				<button onClick={() => setSelected(randNumber)}>Next Anacdotes</button>
+				<button onClick={handleVoteClick}>{votes[selected]} vote</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
