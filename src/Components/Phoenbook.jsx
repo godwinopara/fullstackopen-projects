2f8535b5f9be9d2f3de25c6649adade86edd0d3b
Godwin Opara <godwinopara62@yahoo.com>
2023-06-07 14:29:55 +0100

    feature: Added a feature that allow user to add their phonenumber to the phonebook

--- src/Components/Phoenbook.jsx
+++ src/Components/Phoenbook.jsx
@@ -2,26 +2,29 @@ import { useState } from "react";
 
 const Phoenbook = () => {
 	const [names, setNames] = useState([{ name: "Arto Hellas" }]);
-	const [newUser, setNewUser] = useState("");
+	const [newUser, setNewUser] = useState({ name: "", number: "" });
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const newContact = newUser;
+		const newContact = { ...newUser };
 
 		// check if contact exit on phonebook
-		const userFound = names.find((name) => name.name.toLowerCase() === newContact.toLowerCase());
+		const userFound = names.find((name) => name.name.toLowerCase() === newContact.name.toLowerCase());
 
+		// Add Contact to phonebook
 		if (!userFound) {
-			setNames([...names, { name: newContact }]);
-			setNewUser("");
+			setNames([...names, { name: newContact.name, number: newContact.number }]);
+			setNewUser({ name: "", number: "" });
 			return;
 		}
+		// Show Alert
 		alert(`${newContact} is already added to phonebook`);
-		setNewUser("");
+		setNewUser({ name: "", number: "" });
 	};
 
 	const handleChangeUser = (e) => {
-		setNewUser(e.target.value);
+		const { name, value } = e.target;
+		setNewUser((prevUser) => ({ ...prevUser, [name]: value }));
 	};
 
 	return (
@@ -29,7 +32,9 @@ const Phoenbook = () => {
 			<h2>Phonebook</h2>
 			<form>
 				<div>
-					name: <input value={newUser} onChange={handleChangeUser} type="text" name="name" id="name" />
+					name: <input value={newUser.name} onChange={handleChangeUser} type="text" name="name" id="name" />
+					<br />
+					number: <input value={newUser.number} onChange={handleChangeUser} type="text" name="number" id="number" />
 					<button onClick={handleSubmit} type="submit">
 						add
 					</button>
@@ -38,7 +43,9 @@ const Phoenbook = () => {
 			<h2>Numbers</h2>
 			<div>
 				{names.map((name, i) => (
-					<li key={i}>{name.name}</li>
+					<li key={i}>
+						{name.name} {name.number}
+					</li>
 				))}
 			</div>
 		</div>
