4ab41551267b0ef8201aa088ac39cf6ec1f81445
Godwin Opara <godwinopara62@yahoo.com>
2023-06-08 20:14:02 +0100

    added a post request handler on the backend for adding new contact

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const persons = [
 	{
 		name: "Arto Hellas",
@@ -24,6 +26,11 @@ app.get("/", (req, res) => {
 	res.send("hello world");
 });
 
+app.get("/info", (req, res) => {
+	const time = new Date();
+	res.send(`<div>Phonebook has info for ${persons.length} people <br/> ${time} </div>`);
+});
+
 app.get("/api/persons", (req, res) => {
 	res.json(persons);
 });
@@ -42,11 +49,33 @@ app.delete("/api/persons/:id", (req, res) => {
 	const id = Number(req.params.id);
 	const newPersons = persons.filter((person) => person.id !== id);
 	const person = persons.find((person) => person.id === id);
+
 	if (person) {
 		res.json(newPersons).status(204).end();
 	} else {
 		res.status(404).end();
 	}
+});
+
+app.post("/api/persons", (req, res) => {
+	const newContact = req.body;
+	const existingUser = persons.find((person) => person.name.toLowerCase() === newContact.name.toLowerCase());
+
+	if (!newContact.name) {
+		res.status(400).json({ error: "Name is missing" });
+	}
+	if (!newContact.number) {
+		res.status(400).json({ error: "Number is missing" });
+	}
+	if (existingUser) {
+		res.status(400).json({ error: "User already exists" });
+	}
+
+	console.log(newContact.name);
+
+	console.log(req.headers);
+	console.log(newContact);
+	res.json(newContact);
 });
 
 const port = 5000;
